581f5c5bf34360d01ebfe7a8d88f1e48064e24e3
JGitHub25 <63521762+JGitHub25@users.noreply.github.com>
2024-07-18 16:04:55 +0000

    changename

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link className="button button--secondary button--lg" to="/docs/comenzando/interfaz">
-            Saber más 💻
+           Conócenos 💻
           </Link>
         </div>
       </div>
